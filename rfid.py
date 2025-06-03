a9c2ac120abe030a07461fd6c729fb4567c18726
Keith Hamm <hkhamm@gmail.com>
2025-06-03 12:00:50 -0700

    Update rfid.py to use tkinter

--- rfid.py
+++ rfid.py
@@ -1,106 +1,52 @@
+from tkinter import Tk, Label, LEFT
+from PIL import Image, ImageTk
 import serial
-import wx
+from threading import Timer
 
+start_image = 'images/rfid_main.jpg'
+ten_minutes = 600000
 
-class ImagePanel(wx.Panel):
+# Setup image map
+image_list = 'image_list.txt'
+lines = (line.rstrip('\n') for line in open(image_list))
+image_map = {}
+for line in lines:
+    image_map[line[0]] = line[2:]
 
-    def __init__(self, parent, image):
-    	wx.Panel.__init__(self, parent)
-    	self.frame = parent
-    	self.image = image
-        self.Bind(wx.EVT_CHAR, self.on_keypress)
-        # This line must be uncommented on versions of wxPython prior to 2.9
-    	# self.SetBackgroundStyle(wx.BG_STYLE_CUSTOM)
-        self.Bind(wx.EVT_ERASE_BACKGROUND, self.draw_image)
+# Listen for RFID,
+serial.Serial('/dev/ttyACM0', baudrate=9600)
 
-    def draw_image(self, event):
-    	"""
-    	Draws the image to the panel's background.
-    	"""
-    	dc = event.GetDC()
-    	if not dc:
-    	    dc = wx.ClientDC(self)
-    	    rect = self.GetUpdateRegion().GetBox()
-            dc.SetClippingRect(rect)
-        dc.Clear()
-        dc.DrawBitmap(self.image, 0, 0)
-    	self.SetFocus()
+window = Tk()
+window.attributes('-fullscreen', True)
 
-    def on_keypress(self, event):
-    	"""
-    	Handles key press events.
-    	"""
-        keycode = event.GetKeyCode()
-    	if keycode == wx.WXK_ESCAPE:
-    	    self.frame.close()
-    	else:
-    	    self.frame.switch_image(chr(keycode))
+# Setup start image
+image = Image.open(start_image)
+image = image.resize((1920, 1080))
+image = ImageTk.PhotoImage(image)
+container = Label(window, image=image, command=None)
+container.pack(side=LEFT, expand=1)
 
+def update_image(filename):
+    new_image = Image.open(filename)
+    new_image = new_image.resize((1920, 1080))
+    new_image = ImageTk.PhotoImage(new_image)
+    container.configure(image=new_image)
+    container.image = new_image
 
-class MainFrame(wx.Frame):
+def reset_image():
+    update_image(start_image)
 
-    def __init__(self, parent):
-        wx.Frame.__init__(self, parent)
-        self.ShowFullScreen(True, style=wx.FULLSCREEN_ALL)
+def handle_keypress(event):
+    char = event.char
+    # print('char ' + char)
+    if char == '':
+        return
+    filename = 'images/' + image_map[char]
+    # print('filename ' + filename)
+    update_image(filename)
+    timer = Timer(ten_minutes, reset_image)
+    timer.start()
 
-        # Listen for Arduino
-        # Uncomment when actually listening for Arduino
-        # serial.Serial('/dev/ttyACM0', baudrate=9600)
+window.bind('<KeyPress>', handle_keypress)
 
-        self.image = wx.Bitmap('./images/rfid_main.jpg')
-        self.panel = ImagePanel(self, self.image)
-
-        # Setup image map
-        filename = 'image_list.txt'
-        lines = (line.rstrip('\n') for line in open(filename))
-        self.image_map = {}
-        for line in lines:
-            self.image_map[line[0]] = line[2:]
-
-        # Timer to switch back to start image
-        self.timeout = 600000  # 10 min
-        self.timeout_timer = wx.Timer(self, wx.ID_ANY)
-        self.start_timeout_timer()
-
-    def start_timeout_timer(self):
-        """
-        Timer for the start image.
-        """
-        self.Bind(wx.EVT_TIMER, self.switch_on_timeout, self.timeout_timer)
-        self.timeout_timer.Start(self.timeout)
-
-    def restart_timeout_timer(self):
-        """
-        Stops then restarts the timeout timer.
-        """
-        self.timeout_timer.Stop()
-        self.start_timeout_timer()
-
-    def switch_image(self, char):
-    	"""
-    	Switches the image.
-    	"""
-    	self.image = wx.Bitmap('./images/' + self.image_map[char])
-    	self.panel.image = self.image
-    	self.panel.GetEventHandler().ProcessEvent(wx.EraseEvent())
-        self.restart_timeout_timer()
-
-    def switch_on_timeout(self, event):
-        """
-        Switches the current image frame to the base image.
-        """
-        self.image = wx.Bitmap('./images/rfid_main.jpg')
-    	self.panel.image = self.image
-    	self.panel.GetEventHandler().ProcessEvent(wx.EraseEvent())
-        self.restart_timeout_timer()
-
-    def close(self):
-        """
-        Closes the frame.
-        """
-        self.Destroy()
-
-if __name__ == '__main__':
-    app = wx.App(False)
-    MainFrame(None).Show()
-    app.MainLoop()
+window.mainloop()
